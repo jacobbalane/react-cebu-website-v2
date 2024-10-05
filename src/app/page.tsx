0f0e841d27ca6f7fb4ebf8965e64a3fc4ff0b480
jah09 <jasuhot1@gmail.com>
2024-10-05 16:59:02 +0800

    Worked on Hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import './styles/global.css';
 import Header from './component/Header';
-
+import Hero from './component/Hero';
 export default function Home() {
   return (
     <div>
-      <Header/>
+      <Header />
+      <Hero/>
     </div>
   );
 }
