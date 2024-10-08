27ed02b614891542c9bdc634aade5caefc220026
jah09 <jasuhot1@gmail.com>
2024-10-08 19:54:51 +0800

    Added react icon as bg in Hero section

--- src/app/component/Hero.tsx
+++ src/app/component/Hero.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import group_cutout from "../../assets/images/group-cutout.png";
 import Image from "next/image";
-
+import { MdArrowOutward } from "react-icons/md";
+import { FaReact } from "react-icons/fa";
 const Hero = () => {
   return (
-    <section className="min-h-screen flex-col  flex justify-center items-center  relative bg-gray-100">
-      <div className="px-[100px] mt-10">
-        <div className=" ">
-          <h1 className="text-6xl font-bold  text-center  text-black ">
+    <section className="min-h-screen flex-col flex justify-center items-center relative bg-gray-100">
+      {/* React Icon as Background */}
+      <div className="absolute inset-0 flex justify-center items-center z-0 -mt-[8rem]">
+        <FaReact className="text-[50rem] text-gray-500 opacity-10" />
+      </div>
+
+      {/* Main Content */}
+      <div className="relative z-10 mt-10">
+        <div className="md:px-[100px] lg:px-[120px] xl:px-[130px]">
+          <h1 className="text-6xl font-bold text-center text-black  ">
             Meet and collab with other React developers in Cebu
           </h1>
         </div>
-        <div className="mt-10  first-line:">
+        <div className="mt-10">
           <p className="text-xl text-center text-black">
             React Cebu is a community for developers by developers
           </p>
@@ -21,17 +28,22 @@ const Hero = () => {
             <input
               type="text"
               placeholder="Your Email Address "
-              className="rounded-md  w-80 border p-3 border-black outline-none text-black bg-[#E8E8E8DE"
+              className="rounded-md w-80 border p-3 border-black outline-none text-black bg-[#E8E8E8DE]"
             />
           </div>
           <div>
-            <button className="rounded-md bg-[#61DAFB] w-64 p-3 border-black border text-black">
-              Join Newsletter <span>x</span>
+            <button className="rounded-md bg-[#61DAFB] w-64 p-3 border-black border text-black flex items-center justify-center gap-3">
+              Join Newsletter
+              <span>
+                <MdArrowOutward className="h-6 w-6 text-black" />
+              </span>
             </button>
           </div>
         </div>
       </div>
-      <div className="mt-10">
+
+      {/* Image */}
+      <div className="mt-[50px] relative z-10">
         <Image src={group_cutout} alt="Description of image" className="" />
       </div>
     </section>
