3cbb7e34dcb40079fb094acbe2113c5341b9d312
Jacob <balanejacob1@gmail.com>
2024-10-17 16:54:22 +0800

    fix(alignment): fix content alignment in connect page

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -12,7 +12,7 @@ export default function Connect() {
     <>
       <div className="relative overflow-hidden w-full h-fit flex justify-center">
         <SectionLayout removeDefaultPaddingY={true}>
-          <div className=" flex flex-col items-center md:justify-center h-full space-y-14 md:space-y-20 min-h-fit pt-12 md:py-40">
+          <div className=" flex flex-col items-center md:justify-center h-full space-y-14 md:space-y-20 min-h-fit pt-12 md:pt-0">
             <div className="flex flex-col items-center space-y-8 md:space-y-12">
               <div className="flex-none h-fit md:max-w-[55rem]">
                 <SectionText
@@ -41,12 +41,12 @@ export default function Connect() {
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="grow flex flex-col items-center w-full space-y-3 md:space-y-5 min-h-40">
-              <p className="font-outfitMedium md:text-2xl">
-                Follow or contact us on our social media
-              </p>
-              <Socials invertColor={true} />
+              <div className="flex flex-col items-center w-full space-y-3 md:space-y-5 min-h-40">
+                <p className="font-outfitMedium md:text-2xl">
+                  Follow or contact us on our social media
+                </p>
+                <Socials invertColor={true} />
+              </div>
             </div>
           </div>
         </SectionLayout>
