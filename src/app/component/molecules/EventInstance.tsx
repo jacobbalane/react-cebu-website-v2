130122af67c72e0c30d9182fc32d4bfe873612d1
Jacob <balanejacob1@gmail.com>
2024-10-21 22:48:52 +0800

    feat(event page content): add header text in event page

---
+++ src/app/component/molecules/EventInstance.tsx
@@ -0,0 +1,10 @@
+export default function EventInstance() {
+  const title = "Cebu Hacktoberfest 2024";
+  const date = "2024-10-05";
+  const time = "9:00 AM";
+  const venue = "enspace Cebu (Business Park) Cebu City";
+  const imageUrl =
+    "https://scontent.fmnl17-6.fna.fbcdn.net/v/t39.30808-6/463516483_970373351799851_2175670516131572163_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=75d36f&_nc_ohc=oMnHHEfQvqoQ7kNvgGhr--l&_nc_zt=23&_nc_ht=scontent.fmnl17-6.fna&_nc_gid=AwWjpLuNwHHDJPeG5noyMv7&oh=00_AYAPH_jCDrgzXRksaQNg4Onj0G3rLT5wNEgZCLWyvykg1w&oe=671C142D";
+
+  return <div className=""></div>;
+}
